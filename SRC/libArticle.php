f12e2ec5b21eb806f56e8c02401a308fd7dcb500
onishi <202311ap01_onishi@x-ide.sakura.ne.jp>
2024-02-27 15:30:18 +0900

    #19049 ( Redmine のチケットナンバー )
    ■ 不具合状況
    新規登録時、ファイルマネージャーに保存されていない。
    ■ 修正内容
    ファイルマネージャーで保存するためのコードがコメントアウトされていたので、それを外しました。

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -307,9 +307,8 @@ function subArticleEditComplete()
 		// 新規登録
 		$sql = fnSqlArticleInsert(fnNextNo('ARTICLE'), $article, $room, $keyPlace, $address, $articleNote, $keyBox, $drawing, $sellCharge, $del);
 		$res = mysqli_query($conn, $sql);
-
-		/* $sql = fnSqlFManagerInsert(fnNextNo('FM'),$article,$room,$articleNote,$del);
-		   $res = mysqli_query($conn,$sql); */
+		$sql = fnSqlFManagerInsert(fnNextNo('FM'), $article, $room, $articleNote, $del);
+		$res = mysqli_query($conn, $sql);
 	}
 
 	$_REQUEST['act'] = 'articleSearch';
